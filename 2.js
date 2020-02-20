a1e9a0c874fddee74761a5001fab345d8dd690f9
TanyaKunts <55351746+TanyaKunts@users.noreply.github.com>
2020-02-20 11:28:40 +0300

    Add files via upload

--- 2.js
+++ 2.js
@@ -1,11 +1,7 @@
-const N1=prompt("Введите первое число: ");
-const N2=prompt("Введите второе число: ");
-const N3=prompt("Введите третье число: ");
-const A = Number(N1);
-const B = Number(N2);
-const C = Number(N3);
-if(((A+B)>C) && ((B+C)>A) && ((A+C)>B)){
- alert('Треугольник можно построить!')
-}else{
-alert('Треугольник нельзя построить.')
-}
+const N=prompt("Введите число: ");
+const A = Number(N);
+if ((A%2==0) && (A!=1) && (A!=2)){
+  	alert('Составное число');
+ }else{
+    alert('Простое число');
+ }
--- 6.js
+++ 6.js
@@ -1,7 +1,2 @@
-const Y=prompt("Введите год: ");
-const A = Number(Y);
-if(A%4===0){
-    alert('Год является високосным.')
-}else{
-    alert('Год не является високосным.')
-}
+const K = Number(prompt("Введите число: "));
+alert((K-1)+(K-2));
